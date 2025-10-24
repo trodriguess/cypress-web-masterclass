b9924e1c300e25d569cff92c5911ab61a30f111e
Thais Rodrigues <thaiss.rodriguess@icloud.com>
2025-10-24 10:12:04 -0300

    configurando pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "i5hbsy",
   e2e: {
     defaultCommandTimeout: 10000,
     baseUrl: "http://automationpratice.com.br",
